1f0fb933302275e8883f8a5a1619c24fdcaa6fd8
Andres Camilo Lezcano Restrepo <andrescamilolezcanorestrepo@MacBook-Air-de-Andres.local>
2023-09-04 12:18:46 -0500

    Sprint 1. Se configura la animacion del Splash

---
+++ Pokedex-Platzi/Common/Constans/Constans.swift
@@ -0,0 +1,25 @@
+//
+//  Constans.swift
+//  Pokedex-Platzi
+//
+//  Created by Andres Camilo Lezcano Restrepo on 4/09/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct PokedexColors {
+    
+    static let backgroundBlue = Color(red: 247/255, green: 247/255, blue: 249/255)
+}
+
+struct stringToAssets {
+    
+    static let pikachu = "pikachuAnimated"
+}
+
+struct framesUI {
+    
+    static let width = UIScreen.main.bounds.width
+    static let height = UIScreen.main.bounds.height
+}
